d4ef1c3151f8d2693d99297c07441b80503df9d2
hagit <hagit@ornim.com>
2024-02-08 19:03:27 +0200

    more fixing git issues

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -17,8 +17,8 @@ export function RootCmp() {
             <main>
                 <Routes>
                     {/*{routableRoutes.map(route => <Route key={route.path} exact={true} element={route.component} path={route.path} />)}*/}
-                    <Route path="/ShowCase" element={<StoryIndex/>} />
-                    <Route path="/ShowCase/:username" element={<UserDetails/>} />
+                    <Route path="/" element={<StoryIndex/>} />
+                    <Route path="/:username" element={<UserDetails/>} />
                 </Routes>
             </main>
             <DynamicModal />
