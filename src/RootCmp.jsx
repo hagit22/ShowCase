b9291d1309439a26ccebf6d3956d704ef5a82ee3
hagit <hagit@ornim.com>
2024-02-10 13:47:13 +0200

    change routing for hash router

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -17,8 +17,8 @@ export function RootCmp() {
             <main>
                 <Routes>
                     {/*{routableRoutes.map(route => <Route key={route.path} exact={true} element={route.component} path={route.path} />)}*/}
-                    <Route path="/ShowCase" element={<StoryIndex/>} />
-                    <Route path="/ShowCase/:username" element={<StoryIndex/>} />
+                    <Route path="/" element={<StoryIndex/>} />
+                    <Route path="/:username" element={<StoryIndex/>} />
                 </Routes>
             </main>
             <DynamicModal />
